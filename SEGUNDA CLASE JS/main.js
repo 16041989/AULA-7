36d260858e5c464cc9390422edecb8023985aa7c
martintronconi <martin_tronconi@hotmail.com>
2024-06-17 19:50:51 -0300

    se agregaron metodos

--- SEGUNDA CLASE JS/main.js
+++ SEGUNDA CLASE JS/main.js
@@ -15,3 +15,55 @@ let multipleInfo = [true, 20, "Hola", [1,2,3], {a:1, b:2}]
 
 console.log("la longitud de multipleInfo es: ", multipleInfo.length)
 console.log("el valor del tercer elemento es: ", multipleInfo[3][1])
+
+console.log("-------METODOS DE LOS ARRAYS")
+
+let nombres = ["Pedro", "Juan", "Maria", "Jose"]
+
+console.log(nombres)
+
+console.log("------PUSH()------")
+
+nombres.push("Leo")
+console.log("el nombre agregado es: ", nombres)
+console.log("la longitud de nombres es:", nombres.length)
+
+console.log("-----POP()---------")
+
+let deportes = ["futbol", "tenis", "natacion"]
+let deporteEliminado = deportes.pop()
+console.log("se elimino el ultimo deporte: ", deporteEliminado)
+console.log(deportes)
+
+console.log("-----SHIFT()------")
+
+let colores = ["rojo", "verde", "azul"]
+colores.shift()
+console.log("el primer elemento eliminado es: ", colores)
+
+console.log("-------SPLICE()-------")
+
+let letras = ["a", "b", "c", "d", "e", "f", "g"]
+
+let resultado = letras.splice(2,2)  // elimina a y b 
+console.log("las letras eliminadas son: ", letras)
+console.log("resultado")
+
+let resultado2 = letras.splice(2,0,"1")
+console.log("el resultado de la inserccion es: ", letras)
+
+letras.splice(2,3,"2")
+console.log("el resultado de la inserccion es: ", letras)
+
+console.log("-----JOIN()-----")
+
+let flores =["rosa", "lirio", "orquidea"]
+let frases = letras.join(" - ")
+console.log("devuelve una cadena ", frases)
+
+console.log("-----SLICE()---------")
+
+let verduras = ["lechuga","papa","tomate", "zanahoria"]
+let result = verduras.slice(1,3)
+console.log("las verduras selccionadas son: ", result)
+
