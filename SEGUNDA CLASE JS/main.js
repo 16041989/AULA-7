123830a880cda1a76e0b1e2129fe4904b86e493b
martintronconi <martin_tronconi@hotmail.com>
2024-06-17 15:36:45 -0300

    test

---
+++ SEGUNDA CLASE JS/main.js
@@ -0,0 +1,17 @@
+console.log("------- ARRAYS-------")
+
+console.log("--------HOMOGENEOS------")
+
+let frutas = ["manzana", "pera", "banana"]
+
+// INDICE         1         2        3      
+
+console.log("la longitud de frutas es: ", frutas.length)
+console.log("el ultimo elemento es: ", frutas[2])
+
+console.log("-----HETEROGENEOS-------")
+
+let multipleInfo = [true, 20, "Hola", [1,2,3], {a:1, b:2}]
+
+console.log("la longitud de multipleInfo es: ", multipleInfo.length)
+console.log("el valor del tercer elemento es: ", multipleInfo[3][1])
