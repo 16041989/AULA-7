449f92162e5b0fdd4d3eabc73d5d0b0fd45534d5
martintronconi <martin_tronconi@hotmail.com>
2024-06-24 22:47:18 -0300

    se empieza la clase de funciones

---
+++ 15-FUNCIONES JS/main.js
@@ -0,0 +1,26 @@
+// DECLARATIVAS
+
+function saludar(){
+
+    console.log("hola martin")
+
+}
+
+saludar()
+
+// EXPRESION
+
+const sumar = function(a, b){
+    return a + b 
+}
+console.log(sumar(2, 3))
+
+// PARAMETRO - DIFERENTE ARGUMENTO
+
+// PARAMETRO ES LO QUE RECIBE LA FUNCION POR EJEMPLO a Y b, VARIABLES
+// FUNCTION SUMAR (a, b)
+
+// ARGUMENTO: VALORES REALES QUE LE PASAMOS A LA FUNCION EJEMPLO 2 Y 3
+// SUMAR (2, 3)
+
+
